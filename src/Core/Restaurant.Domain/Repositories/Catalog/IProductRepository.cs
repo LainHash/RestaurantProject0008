8fe8c18c236570e84991cfcda42424d4621b90fc
Lain <chiyomiziwari@gmail.com>
2026-07-02 19:07:46 +0700

    Add Product repository interface and implementation
    Introduced IProductRepository interface for async CRUD operations on Product entities. Implemented ProductRepository using EF Core and RestaurantDbContext to manage Product data access.

---
+++ src/Core/Restaurant.Domain/Repositories/Catalog/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.Domain.Entities.Catalog;
+
+namespace Restaurant.Domain.Repositories.Catalog
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> ToListAsync(CancellationToken cancellationToken = default);
+        Task<Product?> FindAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Product> AddAsync(Product product, CancellationToken cancellationToken = default);
+        Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default);
+        Task DeleteAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/External/Restaurant.Persistence/Repositories/Catalog/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Domain.Entities.Catalog;
+using Restaurant.Domain.Repositories.Catalog;
+using Restaurant.Persistence.Contexts;
+
+namespace Restaurant.Persistence.Repositories.Catalog
+{
+    internal class ProductRepository : IProductRepository
+    {
+        private readonly RestaurantDbContext _context;
+        public ProductRepository(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> ToListAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Products.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Product?> FindAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Products.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+        }
+
+        public async Task<Product> AddAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync(cancellationToken);
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync(cancellationToken);
+            return product;
+        }
+
+        public async Task DeleteAsync(CancellationToken cancellationToken = default)
+        {
+            _context.Products.RemoveRange(_context.Products);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
