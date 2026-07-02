5747186f82bfd2355935bd3f12135e107409d5d3
Lain <chiyomiziwari@gmail.com>
2026-07-02 11:57:36 +0700

    Add MediatR queries/handlers for category retrieval
    Added GetAllCategoriesQuery/Handler and GetCategoryByIdQuery/Handler using MediatR for retrieving categories via ICategoryService. Updated project file to include folder for category commands.

---
+++ src/Core/Restaurant.Application/Features/Catalog/Categories/Queries/GetAll/GetAllCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Restaurant.Application.Models;
+using Restaurant.Contract.DTOs.Catalog.Categories;
+
+namespace Restaurant.Application.Features.Catalog.Categories.Queries.GetAll
+{
+    public record GetAllCategoriesQuery : IRequest<Result<IEnumerable<CategoryResponse>>>
+    {
+    }
+}
---
+++ src/Core/Restaurant.Application/Features/Catalog/Categories/Queries/GetAll/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Restaurant.Application.Models;
+using Restaurant.Application.Services.Catalog;
+using Restaurant.Contract.DTOs.Catalog.Categories;
+
+namespace Restaurant.Application.Features.Catalog.Categories.Queries.GetAll
+{
+    internal class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, Result<IEnumerable<CategoryResponse>>>
+    {
+        private readonly ICategoryService _categoryService;
+        public GetAllCategoriesQueryHandler(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<Result<IEnumerable<CategoryResponse>>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _categoryService.GetAllCategoriesAsync(cancellationToken);
+            return response;
+        }
+    }
+}
