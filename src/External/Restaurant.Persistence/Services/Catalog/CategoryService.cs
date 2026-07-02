02f5ec784f8221ce8c979115a6dc6ba6fe2ef68a
Lain <chiyomiziwari@gmail.com>
2026-07-02 11:49:25 +0700

    Add CategoryService and update DI, DTOs, and project refs
    Added ICategoryService interface and CategoryService implementation for category retrieval. Registered repository and service in DI. Refactored CategoryResponse DTO and updated project references for domain access.

---
+++ src/External/Restaurant.Persistence/Services/Catalog/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using Restaurant.Application.Models;
+using Restaurant.Application.Services.Catalog;
+using Restaurant.Contract.DTOs.Catalog.Categories;
+using Restaurant.Domain.Repositories.Catalog;
+using System.Net;
+
+namespace Restaurant.Persistence.Services.Catalog
+{
+    internal class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<Result<IEnumerable<CategoryResponse>>> GetAllCategoriesAsync(CancellationToken cancellationToken = default)
+        {
+            var categories = await _categoryRepository.ToListAsync(cancellationToken);
+
+            var response = categories.Select(c => new CategoryResponse(c));
+            return Result<IEnumerable<CategoryResponse>>.Succeed(response, Success.Retrieved);
+        }
+
+        public async Task<Result<CategoryResponse>> GetCategoryByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var category = await _categoryRepository.FindAsync(id, cancellationToken);
+            if (category == null)
+            {
+                return Result<CategoryResponse>.Fail(Error.NotFound, HttpStatusCode.NotFound);
+            }
+
+            var response = new CategoryResponse(category);
+            return Result<CategoryResponse>.Succeed(response, Success.Retrieved);
+        }
+    }
+}
--- src/Shared/Restaurant.Contract/DTOs/Catalog/Categories/CategoryResponse.cs
+++ src/Shared/Restaurant.Contract/DTOs/Catalog/Categories/CategoryResponse.cs
@@ -1,10 +1,19 @@
 ﻿using Restaurant.Contract.Abstraction;
+using Restaurant.Domain.Entities.Catalog;
 
 namespace Restaurant.Contract.DTOs.Catalog.Categories
 {
-    public class CategoryResponse : SoftDeletableDTO
+    public class CategoryResponse
     {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
+        public Guid Id {  get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string? Description { get; init; }
+
+        public CategoryResponse(Category category)
+        {
+            Id = category.Id;
+            Name = category.Name;
+            Description = category.Description;
+        }
     }
 }
