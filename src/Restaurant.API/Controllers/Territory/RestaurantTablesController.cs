055e1121787d555da51f27593ecbbc8f7acb4943
Lain <chiyomiziwari@gmail.com>
2026-07-04 12:36:05 +0700

    feat: implement GetById query and controller endpoint for restaurant tables

--- src/Restaurant.API/Controllers/Territory/RestaurantTablesController.cs
+++ src/Restaurant.API/Controllers/Territory/RestaurantTablesController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Application.Features.Territory.Areas.Queries.GetAll;
+using Restaurant.Application.Features.Territory.RestaurantTables.Queries.GetAll;
+using Restaurant.Application.Features.Territory.RestaurantTables.Queries.GetById;
 
 namespace Restaurant.API.Controllers.Territory
 {
@@ -15,10 +17,18 @@ namespace Restaurant.API.Controllers.Territory
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] GetAllAreasQuery query, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetAll([FromQuery] GetAllRestaurantTablesQuery query, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(query, cancellationToken);
             return StatusCode(result.StatusCode, result);
-        } 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetRestaurantTableByIdQuery(id);
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
