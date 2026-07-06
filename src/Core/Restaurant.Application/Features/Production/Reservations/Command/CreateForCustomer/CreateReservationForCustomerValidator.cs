56b5dcc01bb9f412ecf281f096d3c503486a8d0e
Lain <chiyomiziwari@gmail.com>
2026-07-06 20:53:50 +0700

    Add validator for customer reservation command
    Introduced CreateReservationForCustomerValidator using FluentValidation to ensure number of guests is greater than 0 and reservations are made at least 2 hours in advance.

---
+++ src/Core/Restaurant.Application/Features/Production/Reservations/Command/CreateForCustomer/CreateReservationForCustomerValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Restaurant.Application.Features.Production.Reservations.Command.CreateForCustomer
+{
+    internal class CreateReservationForCustomerValidator : AbstractValidator<CreateReservationForCustomerCommand>
+    {
+        public CreateReservationForCustomerValidator()
+        {
+            RuleFor(x => x.Body.NumberOfGuests)
+                .GreaterThan(0).WithMessage("Number of guests must be greater than 0.");
+
+            RuleFor(x => x.Body.ReservationTime)
+                .Must(time => time >= DateTime.Now.AddHours(2))
+                .WithMessage("Reservation must be made at least 2 hours in advance.");
+        }
+    }
+}
