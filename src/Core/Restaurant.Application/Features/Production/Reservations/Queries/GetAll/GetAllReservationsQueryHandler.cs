8553a409c5ea451f600b359edb07593fca894196
Lain <chiyomiziwari@gmail.com>
2026-07-06 15:11:02 +0700

    Add MediatR query and API endpoint for reservations
    Implemented GetAllReservationsQuery and its handler using MediatR to fetch all reservations via IReservationService. Added ReservationsController with a GET endpoint to expose this functionality through the API. Included necessary using directives and namespaces.

---
+++ src/Core/Restaurant.Application/Features/Production/Reservations/Queries/GetAll/GetAllReservationsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Production;
+using Restaurant.Contract.DTOs.Production.Reservations;
+
+namespace Restaurant.Application.Features.Production.Reservations.Queries.GetAll
+{
+    internal class GetAllReservationsQueryHandler : IRequestHandler<GetAllReservationsQuery, Result<IEnumerable<ReservationResponse>>>
+    {
+        private readonly IReservationService _reservationService;
+        public GetAllReservationsQueryHandler(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        public async Task<Result<IEnumerable<ReservationResponse>>> Handle(GetAllReservationsQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _reservationService.GetAllAsync(cancellationToken);
+            return response;
+        }
+    }
+}
---
+++ src/Restaurant.API/Controllers/Production/ReservationsController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Application.Features.Production.Reservations.Queries.GetAll;
+
+namespace Restaurant.API.Controllers.Production
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ReservationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllReservationsQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
