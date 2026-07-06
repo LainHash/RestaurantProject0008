ed34aa2fce8277cd5c09d17a625c7098b802e640
Lain <chiyomiziwari@gmail.com>
2026-07-06 10:58:36 +0700

    Add pagination support to Result<T> class
    Added TotalItems, TotalPages, IndexPage, and PageSize properties to Result<T>. Introduced a new constructor and Succeed overload to handle paginated responses.

--- src/Core/Restaurant.Application/Models/Results/Result.cs
+++ src/Core/Restaurant.Application/Models/Results/Result.cs
@@ -9,6 +9,11 @@ namespace Restaurant.Application.Models.Results
         public int StatusCode { get; }
         public T? Data { get; }
 
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int IndexPage { get; set; }
+        public int PageSize { get; set; }
+
         private Result(T? data, bool isSucceed, string message, HttpStatusCode statusCode)
         {
             Data = data;
@@ -17,8 +22,20 @@ namespace Restaurant.Application.Models.Results
             StatusCode = (int)statusCode;
         }
 
+        private Result(T? data, bool isSucceed, string message, int totalItems, int indexPage, int pageSize, HttpStatusCode statusCode)
+            : this(data, isSucceed, message, statusCode)
+        {
+            TotalItems = totalItems;
+            TotalPages = (int)Math.Ceiling((decimal)totalItems / pageSize);
+            IndexPage = indexPage;
+            PageSize = pageSize;
+        }
+
         public static Result<T> Succeed(T? data, string message, HttpStatusCode statusCode = HttpStatusCode.OK)
             => new(data, true, message, statusCode);
+
+        public static Result<T> Succeed(T? data, string message, int totalItems, int indexPage, int pageSize, HttpStatusCode statusCode = HttpStatusCode.OK)
+            => new(data, true, message, totalItems, indexPage, pageSize, statusCode);
 
         public static Result<T> Fail(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
             => new(default, false, message, statusCode);
