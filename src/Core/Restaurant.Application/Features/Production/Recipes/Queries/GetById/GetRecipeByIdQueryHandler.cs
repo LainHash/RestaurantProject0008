1b7a9b10dbcc4f1e163d62b957ca998a37c66c8e
Lain <chiyomiziwari@gmail.com>
2026-07-07 14:48:28 +0700

    Add GetRecipeById endpoint and query handler
    Added GET api/recipes/{id} endpoint to RecipesController. Introduced GetRecipeByIdQuery and its handler to fetch a recipe by ID using IRecipeService. Updated using directives and namespaces accordingly.

---
+++ src/Core/Restaurant.Application/Features/Production/Recipes/Queries/GetById/GetRecipeByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Production;
+using Restaurant.Contract.DTOs.Production.Recipes;
+
+namespace Restaurant.Application.Features.Production.Recipes.Queries.GetById
+{
+    internal class GetRecipeByIdQueryHandler : IRequestHandler<GetRecipeByIdQuery, Result<RecipeResponse>>
+    {
+        private readonly IRecipeService _recipeService;
+        public GetRecipeByIdQueryHandler(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        public async Task<Result<RecipeResponse>> Handle(GetRecipeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _recipeService.GetByIdAsync(request.Id, cancellationToken);
+            return response;
+        }
+    }
+}
--- src/Restaurant.API/Controllers/Production/RecipesController.cs
+++ src/Restaurant.API/Controllers/Production/RecipesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Application.Features.Production.Recipes.Queries.GetAll;
+using Restaurant.Application.Features.Production.Recipes.Queries.GetById;
 
 namespace Restaurant.API.Controllers.Production
 {
@@ -18,6 +19,14 @@ namespace Restaurant.API.Controllers.Production
         [HttpGet]
         public async Task<IActionResult> GetAll(GetAllRecipesQuery query, CancellationToken cancellationToken)
         {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetRecipeByIdQuery(id);
             var result = await _mediator.Send(query, cancellationToken);
             return StatusCode(result.StatusCode, result);
         }
