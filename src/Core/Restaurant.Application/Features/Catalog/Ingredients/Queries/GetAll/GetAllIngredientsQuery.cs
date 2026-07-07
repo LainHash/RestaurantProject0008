af98c1d27a7e97c5a28434a19289279d2f8e41ad
Lain <chiyomiziwari@gmail.com>
2026-07-07 19:44:01 +0700

    Add MediatR query and API endpoint for ingredients list
    Implemented GetAllIngredientsQuery and handler using IIngredientService. Added IngredientsController with GET endpoint to retrieve all ingredients via MediatR. Updated project file to include new folders for commands and queries organization.

---
+++ src/Core/Restaurant.Application/Features/Catalog/Ingredients/Queries/GetAll/GetAllIngredientsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Contract.DTOs.Catalog.Ingredients;
+
+namespace Restaurant.Application.Features.Catalog.Ingredients.Queries.GetAll
+{
+    public record GetAllIngredientsQuery : IRequest<Result<IEnumerable<IngredientResponse>>>
+    {
+    }
+}
---
+++ src/Restaurant.API/Controllers/Catalog/IngredientsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Application.Features.Catalog.Ingredients.Queries.GetAll;
+
+namespace Restaurant.API.Controllers.Catalog
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public IngredientsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllIngredientsQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
