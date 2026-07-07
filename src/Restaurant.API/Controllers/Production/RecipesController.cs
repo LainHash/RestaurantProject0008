9f02d9eee761f2ab649313f1b0b9c5b0014f17e3
Lain <chiyomiziwari@gmail.com>
2026-07-07 14:36:56 +0700

    Add MediatR-based recipe query and API endpoint
    Added GetAllRecipesQuery and handler for retrieving recipes via MediatR. Introduced RecipesController with a GET endpoint. Updated project structure for recipe features and removed .editorconfig from solution items.

---
+++ src/Restaurant.API/Controllers/Production/RecipesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Application.Features.Production.Recipes.Queries.GetAll;
+
+namespace Restaurant.API.Controllers.Production
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RecipesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(GetAllRecipesQuery query, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
