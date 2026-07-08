c2b25403fcb569663f6361aa9aa57f754f5a1dbe
Lain <chiyomiziwari@gmail.com>
2026-07-08 08:38:47 +0700

    Add update methods and DTOs for ingredients and stocks
    Added Update method to IngredientStock entity. Introduced UpdateIngredientRequest and UpdateIngredientStockRequest DTOs to support updating ingredient and stock details.

---
+++ src/Shared/Restaurant.Contract/DTOs/Catalog/Ingredients/UpdateIngredientRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Contract.DTOs.Catalog.Ingredients
+{
+    public class UpdateIngredientRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ src/Shared/Restaurant.Contract/DTOs/Inventory/IngredientStocks/UpdateIngredientStockRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant.Contract.DTOs.Inventory.IngredientStocks
+{
+    public class UpdateIngredientStockRequest
+    {
+        public decimal UnitPrice { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public decimal StockQuantity { get; set; }
+    }
+}
