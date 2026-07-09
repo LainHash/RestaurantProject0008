28c3186e374a912a536ba56f0734d7b1d58c1d15
Lain <chiyomiziwari@gmail.com>
2026-07-09 20:39:34 +0700

    Add MediatR handlers and API for carts retrieval
    Added GetAllCartsQueryHandler and GetCartByIdQueryHandler using ICartService for data access. Introduced CartsController with endpoints to get all carts and get a cart by ID, leveraging MediatR for query handling and standardized HTTP responses.

---
+++ src/Core/Restaurant.Application/Features/Guests/Carts/Queries/GetById/GetCartByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Guests;
+using Restaurant.Contract.DTOs.Guests.Carts;
+
+namespace Restaurant.Application.Features.Guests.Carts.Queries.GetById
+{
+    internal class GetCartByIdQueryHandler
+        : IRequestHandler<GetCartByIdQuery, Result<CartResponse>>
+    {
+        private readonly ICartService _cartService;
+
+        public GetCartByIdQueryHandler(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<Result<CartResponse>> Handle(GetCartByIdQuery request, CancellationToken cancellationToken)
+        {
+            var specification = new GetCartByIdSpecification(request);
+            var response = await _cartService.GetByIdAsync(specification, cancellationToken);
+            return response;
+        }
+    }
+}
