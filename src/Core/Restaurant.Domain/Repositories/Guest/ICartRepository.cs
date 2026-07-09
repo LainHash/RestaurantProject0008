9ebc5a5d8a73c11db3c2d8c259871d5b040251ed
Lain <chiyomiziwari@gmail.com>
2026-07-09 20:16:09 +0700

    Add Cart repository interface and implementation
    Introduced ICartRepository for async Cart operations and implemented CartRepository using EF Core. Registered the repository with the DI container for application-wide usage.

---
+++ src/Core/Restaurant.Domain/Repositories/Guest/ICartRepository.cs
@@ -0,0 +1,11 @@
+﻿using Restaurant.Domain.Entities.Guests;
+
+namespace Restaurant.Domain.Repositories.Guest
+{
+    public interface ICartRepository
+    {
+        Task<IEnumerable<Cart>> ToListAsync(CancellationToken cancellationToken = default);
+        Task<Cart?> FindAsync(Guid id, CancellationToken cancellationToken = default);
+        Task AddAsync(Cart cart, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/External/Restaurant.Persistence/Repositories/Guest/CartRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Domain.Entities.Guests;
+using Restaurant.Domain.Repositories.Guest;
+using Restaurant.Persistence.Contexts;
+
+namespace Restaurant.Persistence.Repositories.Guest
+{
+    internal class CartRepository : ICartRepository
+    {
+        private readonly RestaurantDbContext _context;
+
+        public CartRepository(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Cart>> ToListAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Cart?> FindAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public Task AddAsync(Cart cart, CancellationToken cancellationToken = default)
+        {
+            _context.Carts.Add(cart);
+            return Task.CompletedTask;
+        }
+    }
+}
