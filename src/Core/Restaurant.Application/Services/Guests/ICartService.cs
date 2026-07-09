c273e2253bc1051bbb97337adc62c8699bda5f85
Lain <chiyomiziwari@gmail.com>
2026-07-09 20:26:41 +0700

    Add ICartService interface and CartService implementation
    Introduced ICartService with methods to retrieve all carts and a cart by ID, returning Result-wrapped DTOs. Implemented CartService using ICartRepository for data access, mapping entities to CartResponse DTOs, and handling not found cases with appropriate Result responses.

---
+++ src/Core/Restaurant.Application/Services/Guests/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using Restaurant.Application.Models.Results;
+using Restaurant.Contract.DTOs.Guests.Carts;
+
+namespace Restaurant.Application.Services.Guests
+{
+    public interface ICartService
+    {
+        Task<Result<IEnumerable<CartResponse>>> GetAllAsync(CancellationToken cancellationToken);
+        Task<Result<CartResponse>> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/External/Restaurant.Persistence/Services/Guests/CartService.cs
@@ -0,0 +1,43 @@
+﻿using Restaurant.Application.Models.Messages;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Guests;
+using Restaurant.Contract.DTOs.Guests.Carts;
+using Restaurant.Domain.Entities.Guests;
+using Restaurant.Domain.Repositories.Guest;
+using System.Net;
+
+namespace Restaurant.Persistence.Services.Guests
+{
+    internal class CartService : ICartService
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public CartService(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task<Result<IEnumerable<CartResponse>>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var carts = await _cartRepository.ToListAsync(cancellationToken);
+
+            var response = carts.Select(x => new CartResponse(x));
+            return Result<IEnumerable<CartResponse>>
+                .Succeed(response, Success.Retrieved);
+        }
+
+        public async Task<Result<CartResponse>> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var cart = await _cartRepository.FindAsync(id);
+            if(cart is null)
+            {
+                return Result<CartResponse>
+                    .Fail(Error.NotFound, HttpStatusCode.NotFound);
+            }
+
+            var response = new CartResponse(cart);
+            return Result<CartResponse>
+                .Succeed(response, Success.Retrieved);
+        }
+    }
+}
