b483cd29c357cddfb41775150bc39259517b9331
Lain <chiyomiziwari@gmail.com>
2026-07-10 09:07:20 +0700

    Add Wishlist repository and DTOs for guest wishlists
    Introduced IWishlistRepository interface and its EF Core implementation for managing Wishlist entities with specification support. Added WishlistItemResponse and WishlistRepsonse DTOs to map Wishlist and WishlistItem domain models to response objects.

---
+++ src/Shared/Restaurant.Contract/DTOs/Guests/WishlistItems/WishlistItemResponse.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant.Domain.Entities.Guests;
+
+namespace Restaurant.Contract.DTOs.Guests.WishlistItems
+{
+    public class WishlistItemResponse
+    {
+        public int Quantity { get; set; }
+        public decimal LineTotal { get; set; }
+        public DateTime AddedAt { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public WishlistItemResponse(WishlistItem wishlistItem)
+        {
+            Quantity = wishlistItem.Quantity;
+            LineTotal = wishlistItem.LineTotal;
+            AddedAt = wishlistItem.AddedAt;
+            ProductName = wishlistItem.Product.Name;
+        }
+    }
+}
