a5d866625357510378e6be695c7a084ab18c0ee4
Lain <chiyomiziwari@gmail.com>
2026-07-10 09:21:42 +0700

    Add wishlist query handlers and API controller
    Added GetAllWishlistsQueryHandler and GetWishlistByIdQueryHandler for retrieving wishlists using MediatR and IWishlistService. Introduced WishlistsController with endpoints to get all wishlists and a wishlist by ID, including result wrapping and status code handling for API responses.

---
+++ src/Core/Restaurant.Application/Features/Guests/Wishlists/Queries/GetAll/GetAllWishlistsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Guests;
+using Restaurant.Contract.DTOs.Guests.Wishlists;
+
+namespace Restaurant.Application.Features.Guests.Wishlists.Queries.GetAll
+{
+    internal class GetAllWishlistsQueryHandler
+        : IRequestHandler<GetAllWishlistsQuery, Result<IEnumerable<WishlistRepsonse>>>
+    {
+        private readonly IWishlistService _wishlistService;
+
+        public GetAllWishlistsQueryHandler(IWishlistService wishlistService)
+        {
+            _wishlistService = wishlistService;
+        }
+
+        public async Task<Result<IEnumerable<WishlistRepsonse>>> Handle(GetAllWishlistsQuery request, CancellationToken cancellationToken)
+        {
+            var specification = new GetAllWishlistsSpecification(request);
+            var response = await _wishlistService.GetAllAsync(specification, cancellationToken);
+            return response;
+        }
+    }
+}
---
+++ src/Core/Restaurant.Application/Features/Guests/Wishlists/Queries/GetById/GetWishlistByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Guests;
+using Restaurant.Contract.DTOs.Guests.Wishlists;
+
+namespace Restaurant.Application.Features.Guests.Wishlists.Queries.GetById
+{
+    internal class GetWishlistByIdQueryHandler
+        : IRequestHandler<GetWishlistByIdQuery, Result<WishlistRepsonse>>
+    {
+        private readonly IWishlistService _wishlistService;
+
+        public GetWishlistByIdQueryHandler(IWishlistService wishlistService)
+        {
+            _wishlistService = wishlistService;
+        }
+
+        public async Task<Result<WishlistRepsonse>> Handle(GetWishlistByIdQuery request, CancellationToken cancellationToken)
+        {
+            var specification = new GetWishlistByIdSpecification(request);
+            var response = await _wishlistService.GetByIdAsync(specification, cancellationToken);
+            return response;
+        }
+    }
+}
