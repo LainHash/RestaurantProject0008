ce41b52c2f4c133ce27e191230de422a6422eb6f
Lain <chiyomiziwari@gmail.com>
2026-07-11 15:10:20 +0700

    Add generic Success<T>/Error<T> messages for entities
    Introduce generic Success<T> and Error<T> classes for type-specific messages in Message.cs. Refactor CategoryService.cs and ProductService.cs to use these for all CRUD operations, improving message clarity. Minor formatting updates in LoginCommand.cs.

---
+++ src/Core/Restaurant.Application/Models/Messages/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Application.Models.Messages
+{
+    public static class Success<T>
+    {
+        public const string Retrieved = $"{nameof(T)} retrived successfully.";
+
+        public const string Created = $"{nameof(T)} created successfully.";
+
+        public const string Updated = $"{nameof(T)} updated successfully.";
+
+        public const string Deleted = $"{nameof(T)} deleted successfully.";
+
+        public const string Restored = $"{nameof(T)} restored successfully.";
+    }
+
+    public static class Error<T>
+    {
+        public const string NotFound = $"{nameof(T)} not found.";
+
+        public const string NotYetDeleted = $"{nameof(T)} not yet deleted.";
+
+        public const string AlreadyDeleted = $"{nameof(T)} already deleted.";
+    }
+}
