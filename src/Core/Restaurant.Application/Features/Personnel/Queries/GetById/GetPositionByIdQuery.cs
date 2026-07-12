0e061391e04075de9210237b4132af0d84ee618f
Lain <chiyomiziwari@gmail.com>
2026-07-12 14:57:18 +0700

    Add GetPositionById endpoint and query handler
    Added GET api/positions/{id} endpoint to retrieve a position by ID. Introduced GetPositionByIdQuery and its handler, integrating with IPositionService. Updated using directives accordingly.

---
+++ src/Core/Restaurant.Application/Features/Personnel/Queries/GetById/GetPositionByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Contract.DTOs.Personnel.Positions;
+
+namespace Restaurant.Application.Features.Personnel.Queries.GetById
+{
+    public record GetPositionByIdQuery(Guid Id)
+        : IRequest<Result<PositionResponse>>
+    {
+    }
+}
--- src/Restaurant.API/Controllers/Personnel/PositionsController.cs
+++ src/Restaurant.API/Controllers/Personnel/PositionsController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Application.Features.Personnel.Queries.GetAll;
+using Restaurant.Application.Features.Personnel.Queries.GetById;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Restaurant.API.Controllers.Personnel
 {
@@ -18,6 +20,14 @@ namespace Restaurant.API.Controllers.Personnel
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] GetAllPositionsQuery query, CancellationToken cancellationToken)
         {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetPositionByIdQuery(id);
             var result = await _mediator.Send(query, cancellationToken);
             return StatusCode(result.StatusCode, result);
         }
