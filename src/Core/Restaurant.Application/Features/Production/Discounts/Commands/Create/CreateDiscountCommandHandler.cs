7eef71b30894b9219a3c2a04d8ac79a396d36f32
Lain <chiyomiziwari@gmail.com>
2026-07-14 20:31:14 +0700

    Add CreateDiscount command and POST endpoint
    Added CreateDiscountCommand and handler using MediatR. Updated DiscountsController with a POST endpoint for creating discounts. Integrated IDiscountService for creation logic and registered necessary folders and using statements.

---
+++ src/Core/Restaurant.Application/Features/Production/Discounts/Commands/Create/CreateDiscountCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Restaurant.Application.Models.Results;
+using Restaurant.Application.Services.Production;
+using Restaurant.Contract.DTOs.Production.Discounts;
+
+namespace Restaurant.Application.Features.Production.Discounts.Commands.Create
+{
+    internal class CreateDiscountCommandHandler
+        : IRequestHandler<CreateDiscountCommand, Result<DiscountResponse>>
+    {
+        private readonly IDiscountService _discountService;
+
+        public CreateDiscountCommandHandler(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        public async Task<Result<DiscountResponse>> Handle(CreateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _discountService.CreateAsync(request.Body, cancellationToken);
+            return response;
+        }
+    }
+}
