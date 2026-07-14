de9f98f2240b2e129f8a13555aa9b6d72e828bb9
Lain <chiyomiziwari@gmail.com>
2026-07-14 20:08:21 +0700

    Add GET endpoint to fetch discount by ID
    Added a new GET api/discounts/{id} endpoint in DiscountsController. Introduced GetDiscountByIdQuery and its handler to retrieve a discount by ID using MediatR and IDiscountService. Updated using statements accordingly.

--- src/Restaurant.API/Controllers/Production/DiscountsController.cs
+++ src/Restaurant.API/Controllers/Production/DiscountsController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.Application.Features.Production.Discounts.Queries.GetAll;
+using Restaurant.Application.Features.Production.Discounts.Queries.GetById;
 
 namespace Restaurant.API.Controllers.Production
 {
@@ -18,6 +19,14 @@ namespace Restaurant.API.Controllers.Production
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] GetAllDiscountsQuery query, CancellationToken cancellationToken)
         {
+            var result = await _mediator.Send(query, cancellationToken);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOne([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            var query = new GetDiscountByIdQuery(id);
             var result = await _mediator.Send(query, cancellationToken);
             return StatusCode(result.StatusCode, result);
         }
